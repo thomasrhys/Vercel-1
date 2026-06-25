e7e4c0db795f970ae5bcbb5d87ce8cfd554b0f99
thomasrhys <56467369+thomasrhys@users.noreply.github.com>
2026-06-25 17:34:14 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
-import { ClerkProvider } from "@clerk/nextjs"
+import { ClerkProvider } from "@clerk/nextjs";
 // Import the Script component for the Head snippet
 import Script from 'next/script'
 
