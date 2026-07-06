bc698b92abad00c02a0d8b0029f16ca2716892f8
thomasrhys <56467369+thomasrhys@users.noreply.github.com>
2026-07-06 10:54:04 +0100

    Fix Microsoft logo colours on login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -31,8 +31,8 @@ function MicrosoftIcon() {
   return (
     <svg viewBox="0 0 23 23" className="h-5 w-5" aria-hidden="true">
       <path fill="#f25022" d="M1 1h10v10H1z" />
-      <path fill="#00a4ef" d="M12 1h10v10H12z" />
-      <path fill="#7fba00" d="M1 12h10v10H1z" />
+      <path fill="#7fba00" d="M12 1h10v10H12z" />
+      <path fill="#00a4ef" d="M1 12h10v10H1z" />
       <path fill="#ffb900" d="M12 12h10v10H12z" />
     </svg>
   );
